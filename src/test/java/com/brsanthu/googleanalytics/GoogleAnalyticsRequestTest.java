018e43da2df0946927f3627483e28ae38f4054e0
Santhosh Kumar <brsanthu@gmail.com>
2017-08-13 23:21:36 -0700

    Fix typo in test method name

--- src/test/java/com/brsanthu/googleanalytics/GoogleAnalyticsRequestTest.java
+++ src/test/java/com/brsanthu/googleanalytics/GoogleAnalyticsRequestTest.java
@@ -18,7 +18,7 @@ public class GoogleAnalyticsRequestTest {
     }
 
     @Test
-    public void testExprimentArgs() throws Exception {
+    public void testExperimentArgs() throws Exception {
         PageViewHit pageViewHit = new PageViewHit().experimentId("1234567890").experimentVariant("some variation");
         assertEquals("1234567890", pageViewHit.experimentId());
         assertEquals("some variation", pageViewHit.experimentVariant());
